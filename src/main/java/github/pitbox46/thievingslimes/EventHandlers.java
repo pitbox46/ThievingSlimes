305313e60a79ef7deb97e0bee74c07ed5e7ac4ed
pitbox46 <pitbox46@gmail.com>
2021-04-03 22:44:08 -0400

    Slimes now decrease durability based on time

--- src/main/java/github/pitbox46/thievingslimes/EventHandlers.java
+++ src/main/java/github/pitbox46/thievingslimes/EventHandlers.java
@@ -8,6 +8,8 @@ import net.minecraft.entity.monster.SlimeEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.LongNBT;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.Hand;
 import net.minecraft.util.NonNullList;
@@ -22,12 +24,11 @@ public class EventHandlers {
         Entity entity = damageEvent.getEntity();
         DamageSource source = damageEvent.getSource();
         if(entity instanceof PlayerEntity && source.getTrueSource() != null && source.getTrueSource().getClass() == SlimeEntity.class) {
-            if(entity.getEntityWorld().getRandom().nextInt(10) != 0) return;
+             if(entity.getEntityWorld().getRandom().nextInt(10) != 0) return;
             SlimeEntity slime = (SlimeEntity) source.getTrueSource();
             if(slime.getHeldItemMainhand() != ItemStack.EMPTY) return;
             PlayerEntity player = (PlayerEntity) entity;
             int selectedSlot = entity.getEntityWorld().getRandom().nextInt(6);
-
             ItemStack itemStack;
             if(selectedSlot <= 3) {
                 itemStack = ((NonNullList<ItemStack>) player.getArmorInventoryList()).get(selectedSlot);
@@ -41,7 +42,13 @@ public class EventHandlers {
                 itemStack = player.getHeldItemOffhand();
                 player.setHeldItem(Hand.OFF_HAND, ItemStack.EMPTY);
             }
+            //Adds tag to help track how much durability that should be taken
+            CompoundNBT itemTag = itemStack.getOrCreateTag();
+            itemTag.putLong("time_absorbed", slime.ticksExisted);
+            itemStack.setTag(itemTag);
             slime.setHeldItem(Hand.MAIN_HAND, itemStack);
+            //Minecraft dropping the item would apply a random durability to it rather than a specific durability
+            slime.setDropChance(EquipmentSlotType.MAINHAND, 0);
         }
     }
 
@@ -50,9 +57,14 @@ public class EventHandlers {
         if(deathEvent.getEntityLiving().getClass() == SlimeEntity.class) {
             SlimeEntity slime = (SlimeEntity) deathEvent.getEntityLiving();
             ItemStack itemStack = slime.getHeldItemMainhand();
-            if(itemStack != ItemStack.EMPTY) {
-                slime.getEntityWorld().addEntity(new ItemEntity(slime.getEntityWorld(), slime.getPosX(), slime.getPosY(), slime.getPosZ(), itemStack));
+            if(itemStack.isDamageable()) {
+                long ticksAbsorbed = slime.ticksExisted - itemStack.getTag().getLong("time_absorbed");
+                for(int i = 0; i < (int)Math.floorDiv(ticksAbsorbed, 100); i++) {
+                    itemStack.damageItem(slime.getRNG().nextInt(6), slime, e -> {});
+                }
+                itemStack.getTag().remove("time_absorbed");
             }
+            slime.getEntityWorld().addEntity(new ItemEntity(slime.getEntityWorld(), slime.getPosX(), slime.getPosY(), slime.getPosZ(), itemStack));
         }
     }
 }
